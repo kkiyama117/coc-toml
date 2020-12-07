943ececb8b65f82ee19b66c86985ac1a23bd16ca
kkiyama117 <k.kiyama117@gmail.com>
2020-12-07 21:17:11 +0900

    version 0.1.2

--- src/syntax/literal/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './boolean';
-export * from './number';
-export * from './string';
-export * from './datetime';
--- src/util.ts
+++
@@ -1,7 +0,0 @@
-import path from 'path';
-
-export function isAbsolutePath(p: string): boolean {
-  return (
-    path.resolve(p) === path.normalize(p).replace(RegExp(path.sep + '$'), '')
-  );
-}
